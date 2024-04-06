29161eb39be1ff5bcabe4f74de2fc5ebbdee8fff
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2024-04-06 02:51:04 -0600

    Added distributable game exe file

--- TheGameOfLife/obj/Release/net8.0/TheGameOfLife.AssemblyInfo.cs
+++ TheGameOfLife/obj/Release/net8.0/TheGameOfLife.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheGameOfLife")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985cabcb43f2f585049425d285e93b583382e0d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheGameOfLife")]
